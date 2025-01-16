4b08ed93010b8136b6bb5b0817e2ab586c7ec2aa
Ahad-Miah <ahadahmedcc@gmail.com>
2025-01-16 22:26:52 +0600

    added basic setup

---
+++ src/Router/Router.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../MainLayout/MainLayout";
+
+
+const Router = createBrowserRouter([
+    {
+        path:'/',
+        element: <MainLayout></MainLayout>
+    }
+])
+   
+
+export default Router;
