47b79184e294450acb166f80c4f144ee8fe1e028
Ahad-Miah <ahadahmedcc@gmail.com>
2025-01-17 01:20:38 +0600

    added static advertisment section

---
+++ src/Pages/Home/Advertise/Advertise.jsx
@@ -0,0 +1,102 @@
+import React from 'react';
+import banner1 from '../../../assets/bi2.avif'
+
+const Advertise = () => {
+    return (
+        <div>
+            <div>
+                <h1 className='text-4xl text-center font-bold mb-5'>Features Properties for sale</h1>
+            </div>
+            <div className='grid grid-cols-1 md:grid cols-3 lg:grid-cols-4 items-center justify-center gap-4'>
+                <div className="group card bg-base-100 shadow-xl overflow-hidden">
+                    <figure className="overflow-hidden">
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes"
+                            className="transition-transform duration-500 group-hover:scale-110"
+                        />
+                    </figure>
+                    <div className="card-body px-2">
+                        <h2 className="card-title">
+                            Property Location
+                            <div className="badge badge-secondary absolute inset-0 left-64 top-5">
+                                NEW
+                            </div>
+                        </h2>
+                        <p>Price Range</p>
+                        <div className="card-actions w-full">
+                            <button className="w-full btn btn-accent">View Details</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="group card bg-base-100 shadow-xl overflow-hidden">
+                    <figure className="overflow-hidden">
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes"
+                            className="transition-transform duration-500 group-hover:scale-110"
+                        />
+                    </figure>
+                    <div className="card-body px-2">
+                        <h2 className="card-title">
+                            Property Location
+                            <div className="badge badge-secondary absolute inset-0 left-64 top-5">
+                                NEW
+                            </div>
+                        </h2>
+                        <p>Price Range</p>
+                        <div className="card-actions w-full">
+                            <button className="w-full btn btn-accent">View Details</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="group card bg-base-100 shadow-xl overflow-hidden">
+                    <figure className="overflow-hidden">
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes"
+                            className="transition-transform duration-500 group-hover:scale-110"
+                        />
+                    </figure>
+                    <div className="card-body px-2">
+                        <h2 className="card-title">
+                            Property Location
+                            <div className="badge badge-secondary absolute inset-0 left-64 top-5">
+                                NEW
+                            </div>
+                        </h2>
+                        <p>Price Range</p>
+                        <div className="card-actions w-full">
+                            <button className="w-full btn btn-accent">View Details</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="group card bg-base-100 shadow-xl overflow-hidden">
+                    <figure className="overflow-hidden">
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes"
+                            className="transition-transform duration-500 group-hover:scale-110"
+                        />
+                    </figure>
+                    <div className="card-body px-2">
+                        <h2 className="card-title">
+                            Property Location
+                            <div className="badge badge-secondary absolute inset-0 left-64 top-5">
+                                NEW
+                            </div>
+                        </h2>
+                        <p>Price Range</p>
+                        <div className="card-actions w-full">
+                            <button className="w-full btn btn-accent">View Details</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default Advertise;
--- src/Pages/Home/HomePage/HomePage.jsx
+++ src/Pages/Home/HomePage/HomePage.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Advertise from '../Advertise/Advertise';
 
 const HomePage = () => {
     return (
         <div>
-            <section className=''>
+            <section className='mb-14'>
                 <Banner></Banner>
+            </section>
+            <section>
+                <Advertise></Advertise>
             </section>
         </div>
     );
