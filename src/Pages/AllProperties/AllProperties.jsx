b879a9ffab40b300b96fdcbabeb581c958c2a8ef
Ahad-Miah <ahadahmedcc@gmail.com>
2025-01-23 13:26:40 +0600

    implement search and sort functionality in all property page

--- src/Pages/AllProperties/AllProperties.jsx
+++ src/Pages/AllProperties/AllProperties.jsx
@@ -1,31 +1,60 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaCheckCircle, FaRegCircle } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import useAxiosSecure from "../../hooks/useAxiosSecure/useAxiosSecure";
 
 const AllProperties = () => {
 
-        const axiosSecure=useAxiosSecure();
-    const { data: properties ,refetch} = useQuery({
-        queryKey: ['properties'],
-        queryFn: async () => {
-          const { data } = await axiosSecure.get(`verifiedProperties`)
-          return data;
+    // const [properties,setProperties]=useState([]);
+        
+        const [sort,setSort]=useState('');
+        
+        // console.log(sort);
+    const axiosSecure = useAxiosSecure();
+    const [search,setSearch]=useState('');
+    const { data: properties, refetch } = useQuery({
+        queryKey: ['properties',search,sort],
+        queryFn:async () => {
+            const { data } =await axios.get(`${import.meta.env.VITE_API_URL}verifiedProperties?search=${search}&sort=${sort}`)
+            return data;
         },
-      })
+    })
     return (
         <section className="py-12 bg-gray-50">
             <div className="mx-auto px-6 lg:px-8">
                 {/* Section Header */}
-                <div className="text-center mb-12">
+                <div className="text-center  mb-12">
                     <h2 className="text-4xl font-bold text-gray-800">
                         All <span className="text-[#C82021]">Properties</span>
                     </h2>
                     <p className="mt-4 text-gray-600">
                         Browse through the latest properties listed by our top real estate agents.
                     </p>
+                    <div className="flex justify-center items-center gap-3">
+                        <label className="input input-bordered flex items-center w-1/2 gap-2">
+                            <input type="text"
+                            onChange={(e)=>setSearch(e.target.value)}
+                            className="grow w-full" placeholder="Search Property" />
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 16 16"
+                                fill="currentColor"
+                                className="h-4 w-4 opacity-70">
+                                <path
+                                    fillRule="evenodd"
+                                    d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                                    clipRule="evenodd" />
+                            </svg>
+                        </label>
+                        <select onChange={(e)=>setSort(e.target.value)} className="select select-error w-full max-w-xs">
+                            <option
+                            disabled selected>Sort By Price</option>
+                            <option value={"assending"}>Low to High</option>
+                            <option value={"desending"}>High to Low</option>         
+                        </select>
+                    </div>
                 </div>
 
                 {/* Properties Grid */}
@@ -42,7 +71,7 @@ const AllProperties = () => {
                                     alt={property?.title}
                                     className="w-full h-48 object-cover group-hover:scale-105 transition-transform duration-300"
                                 />
-                                {property?.verificationStatus==="verified"&& (
+                                {property?.verificationStatus === "verified" && (
                                     <div
                                         className="absolute top-3 right-3 bg-green-100 text-green-600 text-sm font-bold px-2 py-1 rounded-full flex items-center"
                                         data-tooltip="Verified Property"
@@ -72,15 +101,15 @@ const AllProperties = () => {
 
                             {/* Details Button */}
                             <div className="px-4 pb-4">
-                               <Link to={`/details/${property?._id}`}>
-                               <a href className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all w-full bg-red-500 rounded-xl group">
-                                    <span className="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-red-700 rounded group-hover:-mr-4 group-hover:-mt-4">
-                                        <span className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
-                                    </span>
-                                    <span className="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-red-600 rounded-2xl group-hover:mb-12 group-hover:translate-x-0"></span>
-                                    <span className="relative w-full text-center text-white transition-colors duration-200 ease-in-out group-hover:text-white">View Details</span>
-                                </a>
-                               </Link>
+                                <Link to={`/details/${property?._id}`}>
+                                    <a href className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all w-full bg-red-500 rounded-xl group">
+                                        <span className="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-red-700 rounded group-hover:-mr-4 group-hover:-mt-4">
+                                            <span className="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
+                                        </span>
+                                        <span className="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-red-600 rounded-2xl group-hover:mb-12 group-hover:translate-x-0"></span>
+                                        <span className="relative w-full text-center text-white transition-colors duration-200 ease-in-out group-hover:text-white">View Details</span>
+                                    </a>
+                                </Link>
                             </div>
                         </div>
                     ))}
